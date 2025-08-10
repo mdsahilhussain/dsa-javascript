c000364e729971a1279ef84d28c541c51e599543
mdsahilhussain <mdsahil.hussain7@gmail.com>
2025-08-11 00:43:20 +0530

    two pointer question

--- array/two-pointer-question.js
+++ array/two-pointer-question.js
@@ -9,3 +9,17 @@ while (lowPointer != highPointer) {
   highPointer--;
 }
 console.log(arr);
+
+// let arr = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+let pointer_1st = 0;
+let pointer_2nd = 0;
+while (pointer_1st < arr.length) {
+  if (arr[pointer_1st] == 0) {
+    let temp = arr[pointer_1st];
+    arr[pointer_1st] = arr[pointer_2nd];
+    arr[pointer_2nd] = temp;
+    pointer_2nd++;
+  }
+  pointer_1st++;
+}
+console.log(arr);
